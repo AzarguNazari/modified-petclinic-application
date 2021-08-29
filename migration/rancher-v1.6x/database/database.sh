6d5c392823a953529083eb62ae9b251fa78763ce
Hazar Gul Nazari <hazar.nazari@favendo.com>
2021-08-29 22:28:10 +0200

    _refactor

--- migration/rancher-v1.6x/database/database.sh
+++ migration/rancher-v1.6x/database/database.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export RANCHER_URL='http://playground-hazar-1.fvndo.net:8080/v1/projects/1a5'
-export RANCHER_ACCESS_KEY='930C7C93D3EF51484858'
-export RANCHER_SECRET_KEY='QiUuJWyZLGrx2LTYaBDFVdedad4BXB1GLTGdFvYJ'
+export RANCHER_ACCESS_KEY='DD8012BE1F616CC72150'
+export RANCHER_SECRET_KEY='JEoW3sg7hed8fBx59Aiir7np7jNY8KBuT1s675rB'
 
 ../rancher  -w --wait-state healthy up --prune --upgrade --stack database -d  --confirm-upgrade
--- migration/rancher-v1.6x/deploy.sh
+++ migration/rancher-v1.6x/deploy.sh
@@ -2,12 +2,3 @@
 
 (cd database && sh ./database.sh)
 (cd application && sh ./application.sh)
-
-
-#  Install docker server (docker run -d -p 8080:8080 --privileged rancher/server)
-#
-#
-#
-#
-#
-#
