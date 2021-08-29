f2141e60785ea7a42934b82348582ebb0e93755f
Hazar Gul Nazari <hazar.nazari@favendo.com>
2021-08-29 21:58:05 +0200

    _add shell script to copy from local to remote

---
+++ support/copy-files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+scp -r /home/hazarg/master-thesis/master-thesis-work/petclinic-microservices/spring-petclinic-microservices/experiment/docker* fadmin@playground-hazar-1.fvndo.net:/home/fadmin/test
+
+#scp -r ./migration/docker\ swarm fadmin@playground-hazar-1.fvndo.net:/home/fadmin/docker-swarm
